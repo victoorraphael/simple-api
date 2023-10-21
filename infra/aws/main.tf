0684f853d4fb4f890a5a5295b8f684c0ddfe00d2
Raphael Teixeira <raphaelvictor27@gmail.com>
2023-10-21 08:40:05 -0300

    infra: attach sg to ec2

--- infra/aws/main.tf
+++ infra/aws/main.tf
@@ -1,10 +1,14 @@
 resource "aws_instance" "frontend" {
   availability_zone = var.region
-  ami = var.ami
-  instance_type = "t3.medium"
+  ami               = var.ami
+  instance_type     = "t3.medium"
 
   private_ip = "10.0.1.10/24"
-  subnet_id = aws_subnet.simpleapi_frontend_subnet.id
+  subnet_id  = aws_subnet.simpleapi_frontend_subnet.id
+  security_groups = [
+    aws_security_group.allow_all_ssh.id,
+    aws_security_group.simpleapi_frontend_sg.id,
+  ]
 
   tags = {
     Name = "EC2-simpleapi-frontend"
@@ -13,11 +17,15 @@ resource "aws_instance" "frontend" {
 
 resource "aws_instance" "backend" {
   availability_zone = var.region
-  ami = var.ami
-  instance_type = "t3.medium"
+  ami               = var.ami
+  instance_type     = "t3.medium"
 
   private_ip = "10.0.2.10/24"
-  subnet_id = aws_subnet.simpleapi_backend_subnet.id
+  subnet_id  = aws_subnet.simpleapi_backend_subnet.id
+  security_groups = [
+    "${aws_security_group.allow_all_ssh.id}",
+    "${aws_security_group.simpleapi_backend_sg.id}",
+  ]
 
   tags = {
     Name = "EC2-simpleapi-backend"
@@ -26,11 +34,15 @@ resource "aws_instance" "backend" {
 
 resource "aws_instance" "database" {
   availability_zone = var.region
-  ami = var.ami
-  instance_type = "t3.medium"
+  ami               = var.ami
+  instance_type     = "t3.medium"
 
   private_ip = "10.0.3.10/24"
-  subnet_id = aws_subnet.simpleapi_db_subnet.id
+  subnet_id  = aws_subnet.simpleapi_db_subnet.id
+  security_groups = [
+    aws_security_group.allow_all_ssh.id,
+    aws_security_group.simpleapi_database_sg.id
+  ]
 
   tags = {
     Name = "EC2-simpleapi-database"
--- infra/aws/security_group.tf
+++ infra/aws/security_group.tf
@@ -59,3 +59,22 @@ resource "aws_security_group" "simpleapi_database_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "allow_all_ssh" {
+  name   = "allow_all_ssh"
+  vpc_id = aws_vpc.simpleapi_vpc.id
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
